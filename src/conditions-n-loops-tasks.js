c271ec618d16b390687dbbd9b6235d2ee45bed4e
Breslavets Maria <alwayslearn@mail.ru>
2025-01-07 20:30:19 +0300

    feat: solve isPositive and getMaxNumber problem

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,11 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  if (number >= 0) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -38,8 +41,16 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let maxNum = 0;
+  if (a >= b && a >= b) {
+    maxNum = a;
+  } else if (b >= a && b >= c) {
+    maxNum = b;
+  } else {
+    maxNum = c;
+  }
+  return maxNum;
 }
 
 /**
